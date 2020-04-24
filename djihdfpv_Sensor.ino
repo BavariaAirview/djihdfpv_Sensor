4543a079d2ec4f8b38bc38b70454437f54b24696
Bavaria Airview <31274403+BavariaAirview@users.noreply.github.com>
2020-04-24 08:07:56 +0200

    TEST for BUG Speed and DirToHome

--- djihdfpv_Sensor.ino
+++ djihdfpv_Sensor.ino
@@ -172,7 +172,14 @@ void loop()
   if ((uint32_t)(currentMillis_MSP - previousMillis_MSP) >= next_interval_MSP) {
     previousMillis_MSP = currentMillis_MSP;
     GPS_recieve();
-    GPS_calculateDistanceAndDirectionToHome();
+    if (set_home == 0) {
+      CourseToHome();
+      DistToHome();
+    } else {
+      directionToHome = 0;
+      distanceToHome = 0;
+    }
+    //GPS_calculateDistanceAndDirectionToHome();
     send_msp_to_airunit();
 
     general_counter += next_interval_MSP;
@@ -186,7 +193,7 @@ void loop()
     gps_home_lat = gps_lat;
     gps_home_lon = gps_lon;
     gps_home_alt = gps_alt;
-    GPS_calc_longitude_scaling(gps_home_lat);
+    //GPS_calc_longitude_scaling(gps_home_lat);
     set_home = 0;
   }
 }
@@ -386,12 +393,15 @@ void send_msp_to_airunit()
   raw_gps.lon = gps_lon;
   raw_gps.numSat = numSat;
   raw_gps.alt = (int16_t)altitude_msp;
-  raw_gps.groundSpeed = (int16_t)groundspeed;
+  raw_gps.groundSpeed = (int16_t)groundspeed / 3.6;
   msp.send(MSP_RAW_GPS, &raw_gps, sizeof(raw_gps));
 
   //MSP_COMP_GPS
+  //comp_gps.distanceToHome = (int16_t)distanceToHome;
+  //comp_gps.directionToHome = directionToHome;
   comp_gps.distanceToHome = (int16_t)distanceToHome;
   comp_gps.directionToHome = directionToHome;
+
   msp.send(MSP_COMP_GPS, &comp_gps, sizeof(comp_gps));
 
   //MSP_ATTITUDE
@@ -495,7 +505,24 @@ void invert_pos(uint16_t *pos1, uint16_t *pos2)
   *pos2 = tmp_pos;
 }
 
+void CourseToHome() {
+  double directionToHome =
+    TinyGPSPlus::courseTo(
+      gps.location.lat(),
+      gps.location.lng(),
+      gps_home_lat,
+      gps_home_lon);
+}
 
+void DistToHome() {
+  unsigned long distanceToHome =
+    (unsigned long)TinyGPSPlus::distanceBetween(
+      gps.location.lat(),
+      gps.location.lng(),
+      gps_home_lat,
+      gps_home_lon);
+}
+/*
 //from here on code from Betaflight github https://github.com/betaflight/betaflight/blob/c8b5edb415c33916c91a7ccc8bd19c7276540cd1/src/main/io/gps.c
 
 float GPS_scaleLonDown = 1.0f;  // this is used to offset the shrinking longitude as we go towards the poles
@@ -506,23 +533,24 @@ void GPS_calc_longitude_scaling(int32_t lat)
   GPS_scaleLonDown = cos_approx(rads);
 }
 
-#define DISTANCE_BETWEEN_TWO_LONGITUDE_POINTS_AT_EQUATOR_IN_HUNDREDS_OF_KILOMETERS 1.113195f
-#define TAN_89_99_DEGREES 5729.57795f
-// Get distance between two points in cm
-// Get bearing from pos1 to pos2, returns an 1deg = 100 precision
-void GPS_distance_cm_bearing(int32_t *currentLat1, int32_t *currentLon1, int32_t *destinationLat2, int32_t *destinationLon2, uint32_t *dist, int32_t *bearing)
-{
+
+  #define DISTANCE_BETWEEN_TWO_LONGITUDE_POINTS_AT_EQUATOR_IN_HUNDREDS_OF_KILOMETERS 1.113195f
+  #define TAN_89_99_DEGREES 5729.57795f
+  // Get distance between two points in cm
+  // Get bearing from pos1 to pos2, returns an 1deg = 100 precision
+  void GPS_distance_cm_bearing(int32_t *currentLat1, int32_t *currentLon1, int32_t *destinationLat2, int32_t *destinationLon2, uint32_t *dist, int32_t *bearing)
+  {
   float dLat = *destinationLat2 - *currentLat1; // difference of latitude in 1/10 000 000 degrees
   float dLon = (float)(*destinationLon2 - *currentLon1) * GPS_scaleLonDown;
-  *dist = sqrtf(sq(dLat) + sq(dLon)) * DISTANCE_BETWEEN_TWO_LONGITUDE_POINTS_AT_EQUATOR_IN_HUNDREDS_OF_KILOMETERS;
+   dist = sqrtf(sq(dLat) + sq(dLon)) * DISTANCE_BETWEEN_TWO_LONGITUDE_POINTS_AT_EQUATOR_IN_HUNDREDS_OF_KILOMETERS;
 
-  *bearing = 9000.0f + atan2f(-dLat, dLon) * TAN_89_99_DEGREES;      // Convert the output radians to 100xdeg
+   bearing = 9000.0f + atan2f(-dLat, dLon) * TAN_89_99_DEGREES;      // Convert the output radians to 100xdeg
   if (*bearing < 0)
-    *bearing += 36000;
-}
+     bearing += 36000;
+  }
 
-void GPS_calculateDistanceAndDirectionToHome(void)
-{
+  void GPS_calculateDistanceAndDirectionToHome(void)
+  {
   if (gps_home_lat != 0 && gps_home_lon != 0) {      // If we don't have home set, do not display anything
     uint32_t dist;
     int32_t dir;
@@ -533,16 +561,16 @@ void GPS_calculateDistanceAndDirectionToHome(void)
     distanceToHome = 0;
     directionToHome = 0;
   }
-}
+  }
 
-#define M_PIf       3.14159265358979323846f
-#define sinPolyCoef3 -1.666568107e-1f
-#define sinPolyCoef5  8.312366210e-3f
-#define sinPolyCoef7 -1.849218155e-4f
-#define sinPolyCoef9  0
+  #define M_PIf       3.14159265358979323846f
+  #define sinPolyCoef3 -1.666568107e-1f
+  #define sinPolyCoef5  8.312366210e-3f
+  #define sinPolyCoef7 -1.849218155e-4f
+  #define sinPolyCoef9  0
 
-float sin_approx(float x)
-{
+  float sin_approx(float x)
+  {
   int32_t xint = x;
   if (xint < -32 || xint > 32) return 0.0f;                               // Stop here on error input (5 * 360 Deg)
   while (x >  M_PIf) x -= (2.0f * M_PIf);                                 // always wrap input angle to -PI..PI
@@ -551,9 +579,10 @@ float sin_approx(float x)
   else if (x < -(0.5f * M_PIf)) x = -(0.5f * M_PIf) - ((0.5f * M_PIf) + x);
   float x2 = x * x;
   return x + x * x2 * (sinPolyCoef3 + x2 * (sinPolyCoef5 + x2 * (sinPolyCoef7 + x2 * sinPolyCoef9)));
-}
+  }
 
-float cos_approx(float x)
-{
+  float cos_approx(float x)
+  {
   return sin_approx(x + (0.5f * M_PIf));
-}
+  }
+  */
